9934433e4ceabbe8314ac81c75be9bd99f3d174e
genomics-kl <kin.lau@vai.org>
2023-07-05 15:50:51 -0400

    Convert gene IDs to gene symbols in step 4 output.

--- 04_make_counts_table.R
+++ 04_make_counts_table.R
@@ -2,7 +2,7 @@ library(readr)
 library(dplyr)
 library(tibble)
 library(DropletUtils)
-
+library(scuttle)
 
 decoder_file <- "celseq_barcodes.192.txt"
 samples <- c("sample1", "sample2")
@@ -11,7 +11,10 @@ samples <- c("sample1", "sample2")
 dec <- read_tsv(decoder_file, col_names = c("bc_num","bc"))
 
 starsolo_to_table <- function(starsolo_dir, decoder){
-    counts <- as.matrix(counts(read10xCounts(starsolo_dir, col.names=TRUE)))
+
+    sce <- read10xCounts(starsolo_dir, col.names=TRUE)
+    rownames(sce) <- uniquifyFeatureNames(rownames(sce), rowData(sce)$Symbol)
+    counts <- as.matrix(counts(sce))
 
     # rename columns with bc_num
     colnames(counts) <- decoder$bc_num[match(colnames(counts), decoder$bc)]
